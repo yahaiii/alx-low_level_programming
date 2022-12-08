2654a79de989c7ec0dd9f7b65198ddcd88312533
yahaiii <wanekaxa@gmail.com>
2022-12-08 09:28:52 -0800

    code pass betty lint

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
-int main () {
+/**
+ * main - Entry point
+ *
+ * Description: 'outputs string using puts'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
 	char str[52];
 
 	strcpy(str, "\"Programming is like building a multilingual puzzle");
 
 	puts(str);
 
-	return(0);
+	return (0);
 }
