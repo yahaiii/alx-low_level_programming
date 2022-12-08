91ec665b3aa42cdb91f23851e8ed99508834ccca
yahaiii <wanekaxa@gmail.com>
2022-12-08 13:35:26 -0800

    fix typo, an in place of a at typeInt print

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ int main(void)
 	float typeFloat;
 
 	printf("Size of a char: %zu byte(s)\n", sizeof(typeChar));
-	printf("Size of a int: %zu byte(s)\n", sizeof(typeInt));
+	printf("Size of an int: %zu byte(s)\n", sizeof(typeInt));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(typeLong));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(typeLongLong));
 	printf("Size of a float: %zu byte(s)\n", sizeof(typeFloat));
