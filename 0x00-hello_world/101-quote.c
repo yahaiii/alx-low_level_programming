3abbf9240802fcfe531999cc387e2c36dc006ee2
yahaiii <wanekaxa@gmail.com>
2022-12-08 20:44:53 -0800

    fix: escaped problem chars

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: uses write to display text
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+
+	return (1);
+}
