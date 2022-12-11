8e6b6f4d8e1bc386b50ddc6079bfcc299f37f637
yahaiii <wanekaxa@gmail.com>
2022-12-11 13:35:11 -0800

    fix: remove trailing whitespace

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: "Use putchar to print alphabets in both cases"
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ++ch)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ++ch)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
+
