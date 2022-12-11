ae97bbcf22c4e5fccf40996c83287c62f9708f43
yahaiii <wanekaxa@gmail.com>
2022-12-11 12:37:41 -0800

    fix: reduce var name length to please Betty

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,20 +13,21 @@
 int main(void)
 {
 	int n;
-	int lastDigit;
+	int lasDgt;
+	/* lasDgt is last Digit */
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastDigit = n % 10;
-	if (lastDigit > 5)
+	lasDgt = n % 10;
+	if (lasDgt > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lasDgt);
 	}
-	if (lastDigit == 0)
+	if (lasDgt == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+		printf("Last digit of %d is %d and is 0\n", n, lasDgt);
 	}
-	if (lastDigit < 6 && lastDigit != 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+	if (lasDgt < 6 && lasDgt != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lasDgt);
 	return (0);
 }
