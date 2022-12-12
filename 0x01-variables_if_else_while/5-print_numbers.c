7121cfb7991b90bec68892e732d2a24cce85a37d
yahaiii <wanekaxa@gmail.com>
2022-12-12 06:21:02 -0800

    program prints all single digit integers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints all single digits numbers
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		printf("%d",  num);
+		num++;
+	}
+	printf("\n");
+
+	return (0);
+}
