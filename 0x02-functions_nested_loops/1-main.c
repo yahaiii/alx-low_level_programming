0b1fd0009e1d1213752b0bbd73221b4d4e12f35c
yahaiii <wanekaxa@gmail.com>
2022-12-13 08:41:20 -0800

    create function print_alphabet

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - print alphabet
+ *
+ * Return: Always 0
+ */
+void print_alphabet()
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+		_putchar(alp);
+	}
+	_putchar('\n');
+
+	return;
+}
