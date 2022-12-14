2e0e04f0389f052fc73e1c1f0b62adfdd0657d3d
yahaiii <wanekaxa@gmail.com>
2022-12-13 20:54:04 -0800

    program takes int prints up or down to 98 from int

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - takes an int and print all ints after it to 98
+ * @n: starting int
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
